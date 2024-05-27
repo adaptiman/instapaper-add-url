56d79a1c5d35d10c16672a61d79abb2e0d9806e3
adaptiman <david@thesweeneys.org>
2024-05-26 20:46:21 -0500

    about to change methods

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from http.client import HTTPSConnection
 from base64 import b64encode
-import os
+import os, urllib
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,11 +14,18 @@ def basic_auth(USERNAME, USERPASS):
     token = b64encode(f"{USERNAME}:{USERPASS}".encode('utf-8')).decode("ascii")
     return f'Basic {token}'
 
+#This creates the URL to add
+URL = ("https://wilderreport.substack.com/p/the-prairies-frozen-angel")
+TITLE = ("The Prairie's Frozen Angel: Hazel Miner's Sacrifice Echoes through Eternity")
+SELECTION = ("This article was originally told by my friend The Way I Heard It: . Not many outside of North Dakota know this incredible true story. Hazel's story needs telling, and go follow. The Way I Heard It: The prairie wind howls like a famished beast, ripping across the plains with teeth of ice. In this land of stark beauty and brutal winters, survival is a knife's edge, a dance with death where the weak wither and the strong endure. But even the harshest landscapes can't extinguish the flames of courage that flicker in the human heart. Enter Hazel Miner, a fifteen-year-old girl whose story is etched in ice and blood, a testament to the raw, untamed power of love.")
+
+params = urllib.parse.urlencode(({"url": URL, "title": TITLE,"selection": SELECTION}))
+
 #This sets up the https connection
 c = HTTPSConnection("www.instapaper.com")
 #then connect
 headers = { 'Authorization' : basic_auth(USERNAME, USERPASS) }
-c.request('GET', '/', headers=headers)
+c.request("POST", "api/add", params, headers)
 #get the response back
 res = c.getresponse()
 # at this point you could check the status etc
@@ -26,13 +33,6 @@ res = c.getresponse()
 data = res.read()
 print(data)  
 
-# api_url = "https://www.instapaper.com/api/add"
-# url = "https://wilderreport.substack.com/p/the-prairies-frozen-angel"
-# title = ("The Prairie's Frozen Angel: Hazel Miner's Sacrifice Echoes through Eternity")
-# selection = ("This article was originally told by my friend The Way I Heard It: . Not many outside of North Dakota know this incredible true story. Hazel's story needs telling, and go follow. The Way I Heard It: The prairie wind howls like a famished beast, ripping across the plains with teeth of ice. In this land of stark beauty and brutal winters, survival is a knife's edge, a dance with death where the weak wither and the strong endure. But even the harshest landscapes can't extinguish the flames of courage that flicker in the human heart. Enter Hazel Miner, a fifteen-year-old girl whose story is etched in ice and blood, a testament to the raw, untamed power of love.")
-
-# params = {"username": USERNAME, "password": USERPASS, "url": url, "title": title,"selection": selection}
-# urltoadd = urlencode(params)
 # print(urltoadd)
 
 # response = requests.post(api_url, json = urltoadd)
